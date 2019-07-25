3503009cdcaf21508d2daea58016fa5cb81efe20
Arin <arinmsn@gmail.com>
2019-07-24 18:22:31 -0700

    MinMax Algorithm implemented. Alert message is centered.

--- JS/script.js
+++ JS/script.js
@@ -29,7 +29,11 @@ function startGame() {
 
 function turnClick(square) {
     // console.log(square.target.id);
-    turn(square.target.id, huPlayer);
+    // If a square has been clicked, user cannot click again
+    if (typeof origBoard[square.target.id] == 'number') {
+        turn(square.target.id, huPlayer)
+        if (!checkTie()) turn(bestSpot(), aiPlayer);
+    }
 }
 
 function turn(squareId, player) {
@@ -60,10 +64,92 @@ function checkWin(board, player) {
 function gameOver(gameWon) {
     for (let index of winCombos[gameWon.index]) {
         document.getElementById(index).style.backgroundColor =
-        gameWon.player == huPlayer ? "blue" : "red"
+        gameWon.player == huPlayer ? "blue" : "red";
     }
     for (var i=0; i < cells.length; i++) {
-        cells[i].removeEventListener('click', turnClick, false)
+        cells[i].removeEventListener('click', turnClick, false);
     }
+    declareWinner(gameWon.player == huPlayer ? "You won!" : "You lost!");
 }
+
+function declareWinner(who) {
+    document.querySelector(".endgame").style.display = "inline-block";
+    document.querySelector(".endgame .text").innerText = who;
+}
+
+function emptySquares() {
+    return origBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+    // Find and play the empty square
+    //return emptySquares()[0];
+    // MinMax Algorithm
+    return minmax(origBoard, aiPlayer).index;
+}
+
+function checkTie() {
+    if (emptySquares().length == 0) {
+        for (var i = 0; i < cells.length; i++) {
+            cells[i].style.backgroundColor = "green";
+            // Game's over - user cannot click anymore
+            cells[i].removeEventListener('click', turnClick, false);
+        }
+        declareWinner("Tie Game!")
+        return true;
+    }
+    return false;
+}
+
+function minmax(newBoard, player) {
+    var availSpots = emptySquares();
+
+    if (checkWin(newBoard, huPlayer)) {
+        return {score: -10};
+    } else if (checkWin(newBoard, aiPlayer)) {
+        return {score: 10};
+    } else if (availSpots.length === 0) {
+        return {score: 0};
+    }
+
+    var moves = [];
+    for (var i=0; i < availSpots.length; i++) {
+        var move = {};
+        move.index = newBoard[availSpots[i]];
+        newBoard[availSpots[i]] = player;
+
+        if (player == aiPlayer) {
+            var result = minmax(newBoard, huPlayer);
+            move.score = result.score;
+        } else {
+            var result = minmax(newBoard, aiPlayer);
+            move.score = result.score;
+        }
+        // Resets newBoard to what it was before
+        newBoard[availSpots[i]] = move.index;
+        moves.push(move);
+    }
+
+    var bestMove;
+    if(player === aiPlayer) {
+        var bestScore = -10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    } else {
+        var bestScore = 10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    }
+    return moves[bestMove];
+}
+
+
 
